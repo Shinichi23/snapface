200eb0844d02f9cff95432bf086bc6189adc9ec2
Taha <taha-chaoui@hotmail.com>
2022-03-23 23:30:08 +0100

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SnapFaceComponent } from './snap-face/snap-face.component';
+import { InspectorModule } from '@ngneat/inspector';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnapFaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    environment.production ? [] : InspectorModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
